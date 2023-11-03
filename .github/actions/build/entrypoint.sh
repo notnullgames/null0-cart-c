4d85267dc6ad566e5c51e7f2a374fca2077f6803
David Konsumer <konsumer@jetboystudio.com>
2023-11-03 00:03:37 -0700

    working on build action

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # this will be run in /github/actions with your code, inside konsumer/null0 docker
+# you can use it to publish a page and have your cart as a artifact
 
 export GAME="${1}"
 make cart
 cp index.html docs
+
 echo "name=$GAME}" >> $GITHUB_OUTPUT
